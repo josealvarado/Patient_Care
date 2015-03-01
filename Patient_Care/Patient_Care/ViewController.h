2b702764537817561c421d6c9fb9e9ac35b6f477
Jose Alvarado <josealvarado@Joses-MacBook-Pro.local>
2015-03-01 13:51:16 -0800

    Initial Commit
    Initial Commit

---
+++ Patient_Care/Patient_Care/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Patient_Care
+//
+//  Created by Jose Alvarado on 3/1/15.
+//  Copyright (c) 2015 JoseAlvarado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Patient_Care/Patient_Care/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Patient_Care
+//
+//  Created by Jose Alvarado on 3/1/15.
+//  Copyright (c) 2015 JoseAlvarado. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
