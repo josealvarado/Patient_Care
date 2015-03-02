9491a35b0e37a1553078ce7ceeb0c0962d69f3d6
Jose Alvarado <josealvarado@Joses-MacBook-Pro.local>
2015-03-01 17:33:31 -0800

    UI Registration Started
    No backend connectivity

---
+++ Patient_Care/Patient_Care/RegisterViewController.h
@@ -0,0 +1,21 @@
+//
+//  RegisterViewController.h
+//  Patient_Care
+//
+//  Created by Jose Alvarado on 3/1/15.
+//  Copyright (c) 2015 JoseAlvarado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController<NSURLSessionDataDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *textFieldEmaillAdress;
+
+@property (weak, nonatomic) IBOutlet UITextField *textFieldPassword;
+
+@property (weak, nonatomic) IBOutlet UITextField *textfieldPhoneNumber;
+
+- (IBAction)buttonRegisterPressed:(id)sender;
+
+@end
---
+++ Patient_Care/Patient_Care/RegisterViewController.m
@@ -0,0 +1,67 @@
+//
+//  RegisterViewController.m
+//  Patient_Care
+//
+//  Created by Jose Alvarado on 3/1/15.
+//  Copyright (c) 2015 JoseAlvarado. All rights reserved.
+//
+
+#import "RegisterViewController.h"
+
+@interface RegisterViewController ()
+
+@end
+
+@implementation RegisterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)buttonRegisterPressed:(id)sender {
+    
+    NSError *error;
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration delegate:self delegateQueue:nil];
+    NSURL *url = [NSURL URLWithString:@"[JSON SERVER"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url
+                                                           cachePolicy:NSURLRequestUseProtocolCachePolicy
+                                                       timeoutInterval:60.0];
+    
+    [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [request addValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    
+    [request setHTTPMethod:@"POST"];
+    NSDictionary *mapData = [[NSDictionary alloc] initWithObjectsAndKeys: @"Machine", @"username",
+                             @"josealvarado111@gmail.com", @"emailaddress",  @"Password", @"password",
+                             nil];
+    
+    NSData *postData = [NSJSONSerialization dataWithJSONObject:mapData options:0 error:&error];
+    [request setHTTPBody:postData];
+    
+    
+    NSURLSessionDataTask *postDataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        
+    }];
+    
+    [postDataTask resume];
+    
+}
+@end
