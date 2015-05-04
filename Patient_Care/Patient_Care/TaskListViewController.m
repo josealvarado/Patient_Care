9961198e67e71e40c7d3e50fa881be53bf224fe1
josealvarado <josealvarado@Joses-MacBook-Pro-2.local>
2015-05-03 19:17:51 -0700

    Integrated Pull Down To Refresh

--- Patient_Care/Patient_Care/TaskListViewController.m
+++ Patient_Care/Patient_Care/TaskListViewController.m
@@ -22,6 +22,222 @@
     
     tasks = [[NSMutableArray alloc] init];
     
+    // Initialize the refresh control.
+//    self.refreshControl = [[UIRefreshControl alloc] init];
+
+//    [self.refreshControl addTarget:self
+//                            action:@selector(getLatestLoans)
+//                  forControlEvents:UIControlEventValueChanged];
+//    
+    
+    refreshControl = [[UIRefreshControl alloc] init];
+    refreshControl.backgroundColor = [UIColor purpleColor];
+    refreshControl.tintColor = [UIColor whiteColor];
+    [refreshControl addTarget:self action:@selector(getLatestTasks) forControlEvents:UIControlEventValueChanged];
+    
+    [_taskTableView addSubview:refreshControl];
+}
+
+- (void)getLatestTasks {
+    
+    
+    //    NSError *error;
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration delegate:self delegateQueue:nil];
+    
+    //    NSString *params = [NSString stringWithFormat:@"http://52.11.100.150:17000/listtasks?c=8&p=7"];
+    
+    NSString *params = [NSString stringWithFormat:@"http://52.11.100.150:17000/listtasks?c=%@&p=%@", [[[Settings instance].caretaker_list objectAtIndex:0] objectForKey:@"id"], [Settings instance].patient_id];
+    
+    
+    NSURL *url = [NSURL URLWithString:params];
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url
+                                    
+                                                           cachePolicy:NSURLRequestUseProtocolCachePolicy
+                                    
+                                                       timeoutInterval:60.0];
+    
+    [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    [request addValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    
+    [request setHTTPMethod:@"GET"];
+    
+    //    {"patient_id": "1", "caretaker_id": "2", "task": "Go for a walk", "date": "April 8 2015", "time": "10:00"}
+    
+    //    NSLog(@"task %@", [Settings instance].task_name);
+    //    NSLog(@"time %@", [Settings instance].task_date);
+    //
+    //    NSArray* foo = [[Settings instance].task_date componentsSeparatedByString: @" "];
+    //
+    //    NSString* date = [foo objectAtIndex: 0];
+    //    NSString* time = [foo objectAtIndex: 1];
+    
+    
+    //
+    //    NSDictionary *mapData = [[NSDictionary alloc] init ];
+    //    mapData = @{@"patient_id": patientID,
+    //                @"caretaker_id": [Settings instance].caretaker_id,
+    //                @"task": [Settings instance].task_name,
+    //                @"date": date, @"time": time};
+    //
+    //    NSData *postData = [NSJSONSerialization dataWithJSONObject:mapData options:0 error:&error];
+    //
+    //    [request setHTTPBody:postData];
+    
+    
+    NSURLSessionDataTask *postDataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        
+        NSLog(@"data %@", data);
+        
+        NSLog(@"response %@", response);
+        
+        NSLog(@"erorr %@", error);
+        
+        if (!error) {
+            
+            NSLog(@"COrrect");
+            
+            NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *) response;
+            
+            long status_code = (long)[httpResponse statusCode];
+            
+            NSLog(@"response status code: %ld", status_code);
+            
+            NSError* error;
+            
+            NSDictionary* json = [NSJSONSerialization JSONObjectWithData:data
+                                  
+                                                                 options:kNilOptions
+                                  
+                                                                   error:&error];
+            //            NSArray* latestLoans = [json objectForKey:@"loans"];
+            
+            NSLog(@"json: %@", json);
+            
+            NSString* newStr = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+            
+            NSLog(@"str %@", newStr);
+            
+            
+            
+            if (status_code == 202) {
+                
+                //                [self performSegueWithIdentifier:@"PatientHome" sender:sender];
+                
+                
+                //                    [json setValue:_seachTextField.text forKey:@"email"];
+                
+                tasks = [json objectForKey:@"tasks"];
+                
+                int count = (int)[tasks count];
+                
+                [Settings instance].assignedTasksCount = count;
+                
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    
+                    
+                    
+                    //                    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Success"
+                    //
+                    //                                                                    message:@"Task has been added"
+                    //
+                    //                                                                   delegate:nil
+                    //
+                    //                                                          cancelButtonTitle:@"OK"
+                    //
+                    //                                                          otherButtonTitles:nil];
+                    //
+                    //                    [alert show];
+                    
+                    
+                    //                    tasks = ppp;
+                    
+                    NSLog(@"in here with %lu", (unsigned long)[tasks count]);
+                    
+                    //                    [_taskTableView reloadData];
+                    
+                    
+//                    UITableView *tableView = (UITableView *)[self.view viewWithTag:1];
+//                    
+//                    [tableView reloadData];
+                    
+                    [self reloadData];
+                    
+                });
+                
+            } else {
+                
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    
+                    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error"
+                                          
+                                                                    message:@"Failed to get tasks"
+                                          
+                                                                   delegate:nil
+                                          
+                                                          cancelButtonTitle:@"OK"
+                                          
+                                                          otherButtonTitles:nil];
+                    
+                    [alert show];
+                    
+                });
+                
+            }
+        } else {
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"No network connection"
+                                      
+                                                                message:@"You must be connected to the internet to use this app."
+                                      
+                                                               delegate:nil
+                                      
+                                                      cancelButtonTitle:@"OK"
+                                      
+                                                      otherButtonTitles:nil];
+                
+                [alert show];
+                
+            });
+            
+            NSLog(@"what?");
+            
+            
+        }
+        
+    }];
+    
+    [postDataTask resume];
+    
+}
+
+- (void)reloadData
+{
+    // Reload table data
+//    [_taskTableView reloadData];
+    UITableView *tableView = (UITableView *)[self.view viewWithTag:1];
+    
+    [tableView reloadData];
+    
+    // End the refreshing
+    if (refreshControl) {
+        
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        [formatter setDateFormat:@"MMM d, h:mm a"];
+        NSString *title = [NSString stringWithFormat:@"Last update: %@", [formatter stringFromDate:[NSDate date]]];
+        NSDictionary *attrsDictionary = [NSDictionary dictionaryWithObject:[UIColor whiteColor]
+                                                                    forKey:NSForegroundColorAttributeName];
+        NSAttributedString *attributedTitle = [[NSAttributedString alloc] initWithString:title attributes:attrsDictionary];
+        refreshControl.attributedTitle = attributedTitle;
+        
+        [refreshControl endRefreshing];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -163,9 +379,9 @@
                 
                 dispatch_async(dispatch_get_main_queue(), ^{
                     
-                    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Failed"
+                    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error"
                                           
-                                                                    message:@"Something did not work"
+                                                                    message:@"Failed to get tasks"
                                           
                                                                    delegate:nil
                                           
@@ -218,7 +434,28 @@
 */
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+    
+    if ([tasks count] != 0) {
+        tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+
+        return 1;
+    } else {
+        // Display a message when the table is empty
+        UILabel *messageLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height)];
+        
+        messageLabel.text = @"No data is currently available. Please pull down to refresh.";
+        messageLabel.textColor = [UIColor blackColor];
+        messageLabel.numberOfLines = 0;
+        messageLabel.textAlignment = NSTextAlignmentCenter;
+        messageLabel.font = [UIFont fontWithName:@"Palatino-Italic" size:20];
+        messageLabel.lineBreakMode = NSLineBreakByWordWrapping;
+//        [messageLabel sizeToFit];
+        
+        tableView.backgroundView = messageLabel;
+        tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    }
+    
+    return 0;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
